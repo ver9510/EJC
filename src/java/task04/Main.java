9033036312ac8524241621f06b673f24c72536fd
ver9510 <ver9510@yandex.ru>
2017-07-07 19:53:11 +0300

    Done exercise 9.116

--- src/java/task04/Main.java
+++ src/java/task04/Main.java
@@ -6,34 +6,35 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) {
-        Main main=new Main();
-        main.doExercise9_43();
-
-    }
-
-    private void doExercise9_43(){
-        try(BufferedReader reader=new BufferedReader(new InputStreamReader(System.in))){
-            String s1=reader.readLine();
-            //Exercise 9.43
-            StringBuilder builder=new StringBuilder();
-            for (int i = 0; i <s1.length() ; i++) {
-                if(i%2!=0){
-                    builder.append(s1.charAt(i));
-                }
-            }
-            System.out.println(builder.toString());
-
-            /*//Exercise 9.44
-            StringBuilder s2=new StringBuilder();
-            for (int i = s1.length()-1; i >=0; i--) {
-                s2.append(s1.charAt(i));
-            }
-            System.out.println(s2.toString());*/
-        }
-        catch(IOException e){
+        Main main = new Main();
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+            main.doExercise9_43(reader);
+            System.out.println(main.doExercise9_116(reader));
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    //private void doExercise9_
+    private void doExercise9_43(BufferedReader reader) throws IOException {
+        String s1 = reader.readLine();
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < s1.length(); i++) {
+            if (i % 2 != 0) {
+                builder.append(s1.charAt(i));
+            }
+        }
+        System.out.println(builder.toString());
+    }
+
+    private boolean doExercise9_116(BufferedReader reader) throws IOException {
+        boolean res = false;
+        String s1 = reader.readLine();
+        s1 = s1.replace(" ", "");
+        StringBuilder builder = new StringBuilder();
+        for (int i = s1.length() - 1; i >= 0; i--) {
+            builder.append(s1.charAt(i));
+        }
+        res = s1.contentEquals(builder);
+        return res;
+    }
 }
